f4e8b7b5f42e3c74c226bb5ec77894e66a1e53e0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-27 21:36:18 +0900

    Mockup (ch03)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,5 @@
 class TopController < ApplicationController
   def index
-    @message = "おはようございます！"
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title
+    title = "Morning Glory"
+    title = @page_title + " - " + title if @page_title
+    title
+  end
+
+  def menu_link_to(text, path)
+    link_to_unless_current(text, path) { content_tag(:span, text) }
+  end
 end
