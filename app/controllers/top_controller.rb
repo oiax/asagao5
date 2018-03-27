205aa4975d574753e23454f79953dc46fd60824d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-03-27 10:16:56 +0900

    Add top#abount action

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,7 @@ class TopController < ApplicationController
   def index
     @message = "おはようございます！"
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "top#index"
+  get "about" => "top#about", as: "about"
 end
