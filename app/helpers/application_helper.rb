06c13d185a4f1e883af75d424506f0a0a03876b2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-02 23:24:10 +0900

    add_date_unit helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,19 @@ module ApplicationHelper
   def menu_link_to(text, path)
     link_to_unless_current(text, path) { content_tag(:span, text) }
   end
+
+  def add_date_unit(str)
+    suffixes = ["年", "月", "日"]
+
+    replaced = str.gsub(%r{<select .+?>.+?</select>}m) do |element|
+      if md = element.match(/\(([123])i\)/)
+        number = md[1].to_i
+        element + suffixes[number - 1]
+      else
+        element
+      end
+    end
+
+    replaced.html_safe
+  end
 end
