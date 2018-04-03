85923b2a1a799e5c272b85850bfecf127f4d7ecf
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-03 09:40:22 +0900

    Chapter 6b (wip)

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,14 @@
-class Member < ApplicationRecord
+class Member < ActiveRecord::Base
+  validates :number, presence: true,
+    numericality: { only_integer: true,
+      greater_than: 0, less_than: 100, allow_blank: true },
+    uniqueness: true
+  validates :name, presence: true,
+    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+  validates :full_name, length: { maximum: 20 }
+
   class << self
     def search(query)
       rel = order("number")
