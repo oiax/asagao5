d140133b4fc7bf8a9034f123fb26b180d9468e2e
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-05 13:20:05 +0900

    current_member helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  private def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
 end
