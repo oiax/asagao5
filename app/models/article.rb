1b70dbed4ab665418a04533f1770809292074d0c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-04-09 12:25:03 +0900

    [ch09] articles (2) no_expiration

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,16 @@ class Article < ApplicationRecord
   validates :title, :body, :released_at, presence: true
   validates :title, length: { maximum: 80 }
   validates :body, length: { maximum: 2000 }
+
+  def no_expiration
+    expired_at.blank?
+  end
+
+  def no_expiration=(val)
+    @no_expiration = val.in?([true, 1, "1"])
+  end
+
+  before_validation do
+    self.expired_at = nil if @no_expiration
+  end
 end
