5309a8da23398b594eaf19790e6ea41318d59d9a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-05-23 14:38:19 +0900

    Impl. basic layout (ch03)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def page_title
+    title = "Morning Glory"
+    title = @page_title + " - " + title if @page_title
+    title
+  end
+
+  def menu_link_to(text, path, options = {})
+    content_tag :li do
+      link_to_unless_current(text, path, options) do
+        content_tag(:span, text)
+      end
+    end
+  end
 end
