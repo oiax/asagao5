8ddd77677e0b1e0172b1c5cd51f22a583e644afc
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-05-25 21:47:15 +0900

    members#new, #edit

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -15,10 +15,14 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
   end
 
+  # 新規作成フォーム
   def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
   end
 
+  # 編集フォーム
   def edit
+    @member = Member.find(params[:id])
   end
 
   def create
