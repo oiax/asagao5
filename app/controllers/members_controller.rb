0d276cc735ffb4d080204750ae496c65d00e1468
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-05-25 22:16:57 +0900

    members#create, #update, #destroy

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -25,12 +25,31 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
   end
 
+  # 会員の新規登録
   def create
+    @member = Member.new(params[:member]))
+    if @member.save
+      redirect_to @member, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
   end
 
+  # 会員情報の更新
   def update
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member]))
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
+  # 会員の削除
   def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました。"
   end
 end
