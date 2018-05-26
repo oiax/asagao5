b48c8a417c316fc71a41f08bd20feb1eb00d1f6a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-05-26 11:55:41 +0900

    Member: validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,14 @@
 class Member < ApplicationRecord
+  validates :number, presence: true,
+    numericality: { only_integer: true,
+      greater_than: 0, less_than: 100, allow_blank: true },
+    uniqueness: true
+  validates :name, presence: true,
+    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+  validates :full_name, length: { maximum: 20 }
+
   class << self
     def search(query)
       rel = order("number")
