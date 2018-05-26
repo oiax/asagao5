1ae769f01e77dd77f49223bea7f8508f0bf98524
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-05-26 12:18:00 +0900

    Member: email validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,7 +7,8 @@ class Member < ApplicationRecord
     format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true },
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
-  validates :full_name, length: { maximum: 20 }
+  validates :full_name, presence: true, length: { maximum: 20 }
+  validates :email, email: { allow_blank: true }
 
   class << self
     def search(query)
