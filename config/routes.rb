716fb46c7b36fd65cc3c0927ce6eac57f66f8218
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-06-02 21:33:26 +0900

    Section 15-2

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get "lesson/step#{n}(/:name)" => "lesson#step#{n}"
   end
 
-  resources :members do
+  resources :members, only: [:index, :show] do
     get "search", on: :collection
     resources :entries, only: [:index]
   end
@@ -28,5 +28,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: "top#index"
+    resources :members do
+      get "search", on: :collection
+    end
   end
 end
