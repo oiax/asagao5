12e6505eb2a764ae95ddf2b364a62bec5437025c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-06-03 14:48:54 +0900

    Change method names: rescue_NNN => rescue_SSSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,38 +8,39 @@ class ApplicationController < ActionController::Base
   class Forbidden < StandardError; end
 
   if Rails.env.production? || ENV["RESCUE_EXCEPTIONS"]
-    rescue_from StandardError, with: :rescue_500
-    rescue_from ActiveRecord::RecordNotFound, with: :rescue_404
-    rescue_from ActionController::ParameterMissing, with: :rescue_400
+    rescue_from StandardError, with: :rescue_internal_server_error
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
+    rescue_from ActionController::ParameterMissing, with: :rescue_bad_request
   end
 
   rescue_from LoginRequired, with: :rescue_login_required
-  rescue_from Forbidden, with: :rescue_403
+  rescue_from Forbidden, with: :rescue_forbidden
 
   private def login_required
     raise LoginRequired unless current_member
   end
 
-  private def rescue_400(exception)
+  private def rescue_bad_request(exception)
     render "errors/bad_request", status: 400, layout: "error",
       formats: [:html]
   end
 
   private def rescue_login_required(exception)
-    render "errors/login_required", layout: "error", formats: [:html]
+    render "errors/login_required", status: 403, layout: "error",
+      formats: [:html]
   end
 
-  private def rescue_403(exception)
+  private def rescue_forbidden(exception)
     render "errors/forbidden", status: 403, layout: "error",
       formats: [:html]
   end
 
-  private def rescue_404(exception)
+  private def rescue_not_found(exception)
     render "errors/not_found", status: 404, layout: "error",
       formats: [:html]
   end
 
-  private def rescue_500(exception)
+  private def rescue_internal_server_error(exception)
     render "errors/internal_server_error", status: 500, layout: "error",
       formats: [:html]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "top#index"
   get "about" => "top#about", as: "about"
   get "bad_request" => "top#bad_request"
+  get "forbidden" => "top#forbidden"
   get "internal_server_error" => "top#internal_server_error"
 
   1.upto(18) do |n|
